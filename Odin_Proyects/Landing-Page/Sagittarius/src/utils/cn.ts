f87919605af2f056c89a427732c0297121b5ea35
Shivako <codeako@outlook.com>
2026-09-22 13:11:22 -0500

    create: cn.ts

---
+++ Odin_Proyects/Landing-Page/Sagittarius/src/utils/cn.ts
@@ -0,0 +1,6 @@
+/**
+ * Utility: Merge class names conditionally (lightweight clsx alternative)
+ */
+export function cn(...classes: (string | undefined | null | false)[]): string {
+    return classes.filter(Boolean).join(' ');
+}
